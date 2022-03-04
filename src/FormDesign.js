5d3f62e37d445d57f5f61ee4e476d7384aece2f6
Muhammad Faizan <66197447+Muhammadfaizanjanjua109@users.noreply.github.com>
2022-03-05 02:55:33 +0500

    Object In states added

--- src/FormDesign.js
+++ src/FormDesign.js
@@ -1,17 +1,48 @@
 import React, { useState } from 'react'
-
+const Student=[
+    {id:1,name:"faizan"},
+    {id:2,name:"adnan"},
+]
 function FormDesign() {
-    const [input,inputfun]=useState('')
-    const Inputfunction=(e)=>{
-       inputfun(e.target.value);
+ 
+    const [input,inputfun]=useState('');
+    const [itemsData,setItems]=useState(Student);
+
+    const inputData=(e)=>{
+        inputfun(e.target.value);
     }
-  return (
-    <div>
-<input type='text' name='UserName'  onChange={(e)=>{Inputfunction(e)}}/>
-<button>Submit</button>
-<h1>{input}</h1>
-    </div>
-  )
-}
+
+    const InsertData=(event)=>{
+        event.preventDefault();
+        setItems(
+            [
+                ...itemsData,
+                {
+                    id:itemsData.length+1,
+                    name:input
+                }
+            ]
+        )
+        inputfun('');
+    }
+    
+
+
+ return(
+     <>
+     <form onSubmit={(event)=>{InsertData(event)}}>
+         <input name='name' type='text' value={input} onChange={(e)=>{inputData(e)}} />
+     </form>
+     {itemsData.map((input)=>{
+    return <> 
+        <h1>{input.id}</h1>
+    <h1>{input.name}</h1>
+    </>
+})}
+     </>     
+
+
+
+);}
 
 export default FormDesign
